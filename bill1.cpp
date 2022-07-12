2dc309d56732026e8efa615e231fbc65fcfe5900
Dhruvin7256 <107295247+Dhruvin7256@users.noreply.github.com>
2022-07-12 10:04:32 +0530

    Update bill1.cpp

--- bill1.cpp
+++ bill1.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 int main()
 {
-    float rate=20,qty=1000,amt,dis,billamt,gst,netbill,n;
-
-
-    std::cout<<"rate"<<rate<<"\tamt"<<"\tdis"<<"\tbillamt"<<"\t gst"<<"\tnetbill";
-    std::cout<<"\n\tqty"<<qty;
-   
+    float a,b,c;
+    std::cout<<"Enter the value of rate : ";
+    std::cin>>a;
+    std::cout<<"Enter the value of quntity: ";
+    std::cin>>b;
+    std::cout<<"rate   \t  quntity   \tamount\t    discount 5%\t\tbillamount\tgst 18%  \t netbill\n";
+    std::cout<<" "<<a<<"\t "<<b<<"\t\t "<<a*b<<"\t\t"<<(a*b)*0.05<<"\t\t "<<a*b-((a*b)*0.05)<<"\t\t "<<(a*b-((a*b)*0.05))*0.18<<"\t\t "<< a*b-((a*b)*0.05)+((a*b-((a*b)*0.05))*0.18);
+    
+    
+    
 }
